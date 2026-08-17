93bb052bd885ccddc25d61756bc9c35c842e4198
3rdVoyager <palebluedot316@gmail.com>
2026-08-16 21:56:43 -0400

    Add navigation menu to header, update font styles, and add nav hover animations

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,17 @@ const footer = document.querySelector("footer");
 
 header.innerHTML = `
       <h1>4-H Zenith Club</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Programs</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Members</a></li>
+          <li><a href="#">Resources</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
       `;
 
 footer.innerHTML = `
