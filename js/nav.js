3731395968a84bce5cc3c59b23ff6a5601f1c299
3rdVoyager <palebluedot316@gmail.com>
2026-08-16 21:36:48 -0400

    Revamp homepage layout and remove unused pages. Added new sections for club information and updated stylesheets for improved design.

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+header.innerHTML = `
+      <h1>4-H Zenith Club</h1>
+      `;
+
+footer.innerHTML = `
+      <p>&copy; 2026 4-H Zenith Club. All rights reserved.</p>
+    `;
